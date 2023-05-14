81d2298485fcef86e64acba90040700583729c6b
brandford7 <brandfordjunior@gmail.com>
2023-05-14 11:12:45 +0000

    Navbar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,42 @@
-import React from 'react'
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
-import { Navbar, Sidebar, Footer } from './components'
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Navbar, Sidebar, Footer } from "./components";
+import {
+  About,
+  Cart,
+  Checkout,
+  Error,
+  Home,
+  Private,
+  Product,
+  Products,
+} from "./pages";
 
 function App() {
-  return <h4>comfy sloth starter</h4>
+  return (
+    <Router>
+      <Navbar />
+      <Sidebar/>
+      <Routes>
+        <Route exact path="/" element={<Home />} />
+        <Route path="about" element={<About />} />
+        <Route path="cart" element={<Cart />} />
+        <Route path="products" element={<Products />} />
+        <Route path="product/:id" element={<Product />} />
+
+        <Route
+          path="checkout"
+          element={
+            <Private>
+              <Checkout />
+            </Private>
+          }
+        />
+        <Route path="*" element={<Error />} />
+      </Routes>
+      <Footer/>
+    </Router>
+  );
 }
 
-export default App
+export default App;
